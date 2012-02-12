4fa29ffd0b3458c45ba54cafb6c3bc1f2640a28a
Oleg Podsechin <oleg.podsechin@ionsquare.com>
2012-02-12 18:56:46 +0200

    queries etc

--- index.js
+++ index.js
@@ -1,27 +1,89 @@
 //var base64 = require("common-utils/base64");
+var objects = require("common-utils/objects");
 var Application = require("stick").Application;
-var mongo = new (require("mongo-sync").Server)();
-
+var mongo = require("mongo-sync");
+var ObjectId = mongo.ObjectId;
 var app = exports.app = Application();
 
-var db = mongo.db("tokenize");
+var db = new mongo.Server().db("tokenize");
 
 app.configure("notfound", require("./json"), "params", "route");
+app.json.id = "objectId";
+
+function wrap(e) {
+	if (!e) {
+		throw {
+			notfound : true
+		};
+	}
+	e.objectId = e._id;
+	delete e._id;
+	return e;
+}
+
+// TODO count, order
+// TODO authentication
+// TODO counters, data types, relational queries
+// TODO users
+// TODO files
+// TODO geopoints
 
 app.get("/classes/:collection", function(request, collection) {
-	return db.getCollection(collection).find().toArray().map(function(e) {
-		e.objectId = e._id;
-		delete e._id;
-		return e;
-	});
+	var limit = request.params.limit || 100;
+	if (limit > 1000)
+		limit = 1000;
+	return {
+		results : db.getCollection(collection).find(
+				request.params.where ? JSON.parse(request.params.where) : {})
+				.skip(request.params.skip || 0).limit(limit).toArray()
+				.map(wrap)
+	};
+});
+
+app.get("/classes/:collection/:id", function(request, collection, id) {
+	return wrap(db.getCollection(collection).findOne({
+		_id : new ObjectId(id)
+	}));
 });
 
 app.post("/classes/:collection", function(request, collection) {
-	var object = JSON.parse(request.input.read().decodeToString());
+	var object = request.body;
 	object.createdAt = object.updatedAt = new Date();
 	var id = db.getCollection(collection).save(object)._id;
 	return {
-		objectId: id,
-		createdAt: object.createdAt
+		objectId : id,
+		createdAt : object.createdAt
+	}
+});
+
+app.put("/classes/:collection/:id", function(request, collection, id) {
+	// we're really doing a PATCH here
+	var objectId = new ObjectId(id);
+	var object = db.getCollection(collection).findOne({
+		_id : new ObjectId(id)
+	});
+	if (!object) {
+		throw {
+			notfound : true
+		};
+	}
+	object = objects.merge(object, request.body);
+	object.updatedAt = new Date();
+	delete object._id;
+	db.getCollection(collection).update({
+		_id : objectId
+	}, object);
+	return {
+		updatedAt : object.updatedAt
+	};
+});
+
+app.del("/classes/:collection/:id", function(request, collection, id) {
+	if (!db.getCollection(collection).remove({
+		_id : new ObjectId(id)
+	})) {
+		throw {
+			notfound : true
+		};
 	}
 });
--- json.js
+++ json.js
@@ -1,39 +1,70 @@
+var objects = require("common-utils/objects");
+
+function requestUrl(request) {
+	//TODO handle case when we're behind a proxy
+	return request.scheme + '://' + request.host
+			+ (request.port == 80 ? '' : ':' + request.port) + request.scriptName
+			+ request.pathInfo;
+}
+
 exports.middleware = function(next, app) {
+	app.json = {};
 	var headers = {
-		"Content-Type": "application/json"
+		"Content-Type" : "application/json"
 	};
 	var codes = {
-		GET: 200,
-		HEAD: 200,
-		//TODO include Location header
-		POST: 201,
-		PUT: 200,
-		DELETE: 204
+		GET : 200,
+		HEAD : 200,
+		POST : 201,
+		PUT : 200,
+		DELETE : 204
 	};
 	return function(request) {
+		var body = request.input.read().decodeToString();
+		if (body) {
+			try {
+				request.body = JSON.parse(body);
+			} catch (e) {
+				return {
+					status : 400,
+					headers : headers,
+					body : [ JSON.stringify({
+						error : 400,
+						reason : e.message
+					}), "\n" ]
+				}
+			}
+		}
 		try {
 			var result = next(request);
+			var h = headers;
+			if (request.method == "POST") {
+				h = objects.merge(h, {
+					"Location" : requestUrl(request) + '/' + result[app.json.id]
+				});
+			}
 			return {
-				status: codes[request.method],
-				headers: headers,
-				body: result && request.method != "HEAD" ? [JSON.stringify(result),
-						"\n"] : []
+				status : codes[request.method],
+				headers : h,
+				body : result && request.method != "HEAD" ? [
+						JSON.stringify(result), "\n" ] : []
 			};
-		} catch(e) {
-			if(e.notfound) {
+		} catch (e) {
+			if (e.notfound) {
 				return {
-					status: 404,
-					headers: {},
-					body: []
+					status : 404,
+					headers : {},
+					body : []
 				};
 			} else {
+				console.warn(e.stack);
 				return {
-					status: 503,
-					headers: headers,
-					body: [JSON.stringify({
-						error: 503,
-						reason: e.message
-					}), "\n"]
+					status : 503,
+					headers : headers,
+					body : [ JSON.stringify({
+						error : 503,
+						reason : e.message
+					}), "\n" ]
 				};
 			}
 		}
